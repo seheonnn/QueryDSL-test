1199e91a0d08a67c3771752369d890f5b190951b
seheonnn <ho78901@naver.com>
2024-02-25 23:15:27 +0900

    refactor: 참고자료 주석 추가

--- src/main/java/com/example/QueryDSLtest/repository/ArticleRepositoryImpl.java
+++ src/main/java/com/example/QueryDSLtest/repository/ArticleRepositoryImpl.java
@@ -18,6 +18,8 @@ public class ArticleRepositoryImpl implements ArticleRepository {
 	private final EntityManager entityManager;
 	private final ArticleJpaRepository articleJpaRepository;
 
+	// JPQL vs QueryDSL : https://www.inflearn.com/questions/38771/querydsl%EA%B3%BC-jpql%EC%9D%84-%EC%84%A0%ED%83%9D%ED%95%98%EB%8A%94-%EC%B0%A8%EC%9D%B4%EA%B0%80-%EA%B6%81%EA%B8%88%ED%95%A9%EB%8B%88%EB%8B%A4
+	// repository 추상화 정도 : https://www.inflearn.com/questions/31415/spring-jpa-repository
 	@Override
 	public List<Article> searchArticles(String keyword) {
 		QArticle article = QArticle.article;
